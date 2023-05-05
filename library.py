6fd6b905b5deed20d68202aef3951c6d0b192719
Rantoryu <dominikmigrala@gmail.com>
2023-05-05 21:58:18 +0200

    Added Delete Functionality

--- library.py
+++ library.py
@@ -1,46 +1,49 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="user",
-  password=""
+    host="localhost",
+    user="user",
+    password="",
+    database="library"
 )
-
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE IF NOT EXISTS library")
-mycursor.execute("USE library")
-mycursor.execute('''CREATE TABLE IF NOT EXISTS books
-                 (id INT PRIMARY KEY AUTO_INCREMENT,
-                 title VARCHAR(255),
-                 author VARCHAR(255),
-                 year INT(4))''')
+mycursor.execute('''
+    CREATE TABLE IF NOT EXISTS books (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        title VARCHAR(255),
+        author VARCHAR(255),
+        year INT(4)
+    )
+''')
 
 def add_book(title, author, year):
-    mycursor.execute('''INSERT INTO books (title, author, year)
-                      VALUES (%s, %s, %s)''', (title, author, year))
+    mycursor.execute('''
+        INSERT INTO books (title, author, year)
+        VALUES (%s, %s, %s)
+    ''', (title, author, year))
     mydb.commit()
     print(mycursor.rowcount, "book added successfully")
 
 def get_all_books():
-    mycursor.execute('''SELECT * FROM books''')
+    mycursor.execute('SELECT * FROM books')
     rows = mycursor.fetchall()
     for row in rows:
         print(row)
 
 def update_book(id, title=None, author=None, year=None):
-    query = '''UPDATE books SET '''
+    query = 'UPDATE books SET '
     values = []
     if title:
-        query += '''title = %s, '''
+        query += 'title = %s, '
         values.append(title)
     if author:
-        query += '''author = %s, '''
+        query += 'author = %s, '
         values.append(author)
     if year:
-        query += '''year = %s, '''
+        query += 'year = %s, '
         values.append(year)
-    query = query[:-2] + ''' WHERE id = %s'''
+    query = query[:-2] + ' WHERE id = %s'
     values.append(id)
 
     mycursor.execute(query, values)
@@ -51,12 +54,22 @@ def update_book(id, title=None, author=None, year=None):
     else:
         print(mycursor.rowcount, 'book updated successfully')
 
+def delete_book(id):
+    mycursor.execute('DELETE FROM books WHERE id = %s', (id,))
+    mydb.commit()
+
+    if mycursor.rowcount == 0:
+        print('No book found with id', id)
+    else:
+        print(mycursor.rowcount, 'book deleted successfully')
+
 def main():
     while True:
         print('1. Add book')
         print('2. View all books')
         print('3. Update book')
-        print('4. Quit')
+        print('4. Delete book')
+        print('5. Quit')
         choice = input('Enter your choice: ')
 
         if choice == '1':
@@ -73,6 +86,9 @@ def main():
             year = input('Enter new year (press enter to skip): ')
             update_book(id, title, author, year)
         elif choice == '4':
+            id = input('Enter book id: ')
+            delete_book(id)
+        elif choice == '5':
             break
         else:
             print('Invalid choice')
